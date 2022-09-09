10236caf629261efb160fb00544738e641fa769d
Artem <temka6397@mail.ru>
2022-09-09 20:40:20 +0500

    first homework

---
+++ dz1.py
@@ -0,0 +1,48 @@
+# Задача 1
+
+# day_number = int(input("Введите номер дня недели: "))
+# if day_number != 6 and day_number != 7:
+#     print("Нет")
+# else:
+#     print("Да")
+
+# Задача 3
+
+# x = int(input("Введите X:"))
+# y = int(input("Введите Y:"))
+# if x > 0 and y > 0:
+#     print("1 четверть")
+# else:
+#     if x > 0 and y < 0:
+#         print ("4 четверть")
+#     else:
+#         if x < 0 and y > 0:
+#             print("2 четверть")
+#         else:
+#             print("3 четверть")
+
+
+# Задача №4
+
+# number_quarter = int(input("Введи номер четверти: "))
+# if number_quarter > 4:
+#     print("Учи математику")
+# else:
+#    if number_quarter == 1:
+#     print("x>0,y>0")
+#    else:
+#     if number_quarter == 2:
+#         print("x<0,y>0")
+#     else:
+#         if number_quarter == 3:
+#            print("x<0,y<0")
+#         else:
+#            print("x>0,y<0")
+
+# # Задача №5
+# from math import sqrt
+# a1 = int(input("Введи координату х1: "))
+# a2 = int(input("Введи координату х1: "))
+# b1 = int(input("Введи координату y1: "))
+# b2 = int(input("Введи координату y2: "))
+# print((sqrt(pow(a2-a1,2)+pow(b2-b1,2))))
