3d7c67c73d2b9921afaf4892fd3dacc0d48845e3
Artem <temka6397@mail.ru>
2022-09-17 12:16:08 +0500

    Third homework ver_3

--- dz3.py
+++ dz3.py
@@ -25,10 +25,38 @@
 #     arr2.append(arr[i] * arr[- 1 - i])
 # print(arr2)
 
-from random import randint
-from math import floor
+# Задача №3 
 
-first_arr = []
-for i in range(0, randint(1, 10)):
-    first_arr.append(randint(0, 20) + randint(0,100)/100)
-print(first_arr)
+# from random import randint
+
+# first_arr = []
+# for i in range(0, randint(1, 10)):
+#     first_arr.append(randint(0, 20) + randint(0,100)/100)
+# print(first_arr)
+
+# def dif(first_arr):
+#     max_min =[]
+#     for i in range(len(first_arr)):
+#         max_min.append(first_arr[i]%1)
+#     return max(max_min) - min(max_min)
+# print(first_arr, ' -> ', round(dif(first_arr),2))
+
+# # Задача №4
+
+
+# a = int(input("Введите число: "))
+# num = bin(a) 
+# print(num[2:]) 
+
+# Задача №5
+
+# k = int(input('Введите число k: '))
+# fib_list = [0]*(k*2+1)
+# fib_list[k] = 0
+# fib_list[k+1] = 1
+# for i in range(k+2, len(fib_list)):
+#     fib_list[i] = fib_list[i-2] + fib_list[i-1]
+
+# for i in range(k, -1, -1):
+#     fib_list[i] = fib_list[i+2] - fib_list[i+1]
+# print(fib_list)
